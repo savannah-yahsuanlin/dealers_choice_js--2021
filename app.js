deed70ee7eb76ac5bf2d4ef14d3c995bdcebeeb8
yahsuanlinus <savannahwang75@gmail.com>
2021-11-02 16:42:45 -0400

    add express, morgan, nodemon

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
