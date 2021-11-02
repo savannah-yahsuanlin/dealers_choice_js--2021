e9d18b3db0a286423dd2f82c2825ae7d949f21c8
yahsuanlinus <savannahwang75@gmail.com>
2021-11-02 18:10:32 -0400

    add css, image

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
+const beerList = require("./views/beerList")
+const beer = require("./beer");
 const app = express();
 
 app.use(morgan('dev'));
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  const posts = beer.list();
+  res.send(beerList(posts));
+});
 
 const PORT = 3000;
 
